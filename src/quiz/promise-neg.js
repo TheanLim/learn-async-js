0c5ad56eab79942b8eda8dcab3d7f1d54514e9c9
Thean Lim <limxx431@umn.edu>
2024-02-27 18:05:08 -0500

    Implemented negative number concurrent logging in a 2D array

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,25 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function logNegativeARow(row){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (row.some(num => num < 0)){
+                resolve(row);
+            } else{
+                reject(new Error('No negative numbers found in the row'))
+            }
+        }, 0);
+    });
+}
+
+const promiseArr = array2D.map(row => logNegativeARow(row))
+
+Promise.any(promiseArr)
+    .then((response) => {
+        console.log('At least a negative number is found at:', response);
+    })
+    .catch((error) => {
+        console.error('No negative numbers are found in the array');
+    });
